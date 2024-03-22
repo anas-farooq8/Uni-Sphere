35bd7b4acc95e2ecde9b9bc7650b9d0af84bb626
Muhammad Anas Farooq <anasfarooq8888@gmail.com>
2024-03-22 14:59:52 +0500

    Initial

---
+++ Uni-Sphere/Pages/login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Uni_Sphere.Pages
+{
+    public class loginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
